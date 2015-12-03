14b9644cb9c19137e81cfa9a8e1abe380177d51c
Willy Snow <sid@feelthedrive.ru>
2015-12-04 01:21:06 +0300

    main page

--- yii2app/views/site/index.php
+++ yii2app/views/site/index.php
@@ -161,24 +161,24 @@ foreach ($reviewsData as $review) {
         <div class="row main-page__block-safety-content">
             <?php
             $teasers = [
-                ['id' => 'flexible', 'title' => 'Учебные заведения', 'description' => 'Школы, ВУЗы, детские сады'],
-                ['id' => 'living', 'title' => 'Жилые помещения', 'description' => 'Парки, дома отдыха, ночные клубы'],
-                ['id' => 'auto', 'title' => 'Автотранспортные предприятия', 'description' => 'Автостоянки, автосалоны, гаражи, АЗС'],
-                ['id' => 'party', 'title' => 'Массовые мероприятия', 'description' => 'Концерты, свадьбы, соревнования'],
-                ['id' => 'vacation', 'title' => 'Места досуга и отдыха', 'description' => 'Парки, дома отдыха, ночные клубы'],
-                ['id' => 'offices', 'title' => 'Производственные<br/> предприятия и офисы', 'description' => 'Заводы, фабрики, бизнес-центры'],
-                ['id' => 'store', 'title' => 'Магазины', 'description' => 'Торговые центры, супермаркеты'],
-                ['id' => 'medical', 'title' => 'Медицинские учреждения', 'description' => 'Поликлиники, больницы, аптеки'],
-                ['id' => 'buildings', 'title' => 'Строительные объекты', 'description' => 'Стройплощадки'],
+                ['id' => 'flexible', 'alias' => 'page_30', 'title' => 'Учебные заведения', 'description' => 'Школы, ВУЗы, детские сады'],
+                ['id' => 'living', 'alias' => 'page_20', 'title' => 'Жилые помещения', 'description' => 'Парки, дома отдыха, ночные клубы'],
+                ['id' => 'auto', 'alias' => 'page_151', 'title' => 'Автотранспортные предприятия', 'description' => 'Автостоянки, автосалоны, гаражи, АЗС'],
+                ['id' => 'party', 'alias' => 'page_42', 'title' => 'Массовые мероприятия', 'description' => 'Концерты, свадьбы, соревнования'],
+                ['id' => 'vacation', 'alias' => 'page_99', 'title' => 'Места досуга и отдыха', 'description' => 'Парки, дома отдыха, ночные клубы'],
+                ['id' => 'offices', 'alias' => 'page_31', 'title' => 'Производственные<br/> предприятия и офисы', 'description' => 'Заводы, фабрики, бизнес-центры'],
+                ['id' => 'store', 'alias' => 'page_32', 'title' => 'Магазины', 'description' => 'Торговые центры, супермаркеты'],
+                ['id' => 'medical', 'alias' => 'page_43', 'title' => 'Медицинские учреждения', 'description' => 'Поликлиники, больницы, аптеки'],
+                ['id' => 'buildings', 'alias' => 'page_34', 'title' => 'Строительные объекты', 'description' => 'Стройплощадки'],
             ];
             foreach( $teasers as $item){?>
-                <div class="col-md-4 col-sm-6 col-xs-12 teaser">
+                <a href="<?=Url::to([$route, 'alias' => $item['alias']])?>" class="col-md-4 col-sm-6 col-xs-12 teaser">
                     <div class="teaser-image teaser-image-<?=$item['id']?>"></div>
                     <div class="teaser-caption">
-                        <div class="teaser-caption-header text-red"><?=$item['title']?></div>
+                        <div class="teaser-title text-red"><?=$item['title']?></div>
                         <?=$item['description']?>
                     </div>
-                </div>
+                </a>
             <?php } ?>
         </div>
     </div>
