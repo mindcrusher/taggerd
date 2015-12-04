0131d5ac15d64cfd684ae67aa3befeccceb3d8bd
Willy Snow <sid@feelthedrive.ru>
2015-12-04 07:04:41 +0300

    seo provided

--- yii2app/views/site/info.php
+++ yii2app/views/site/info.php
@@ -1,5 +1,13 @@
 <?php
 $this->title = !empty($page->seo_title) ? $page->seo_title : $page->title;
+$this->registerMetaTag([
+    'name' => 'keywords',
+    'content' => $page->seo_keywords
+]);
+$this->registerMetaTag([
+    'name' => 'description',
+    'content' => $page->seo_description
+]);
 ?>
 <div class="<?=Yii::$app->request->isAjax ? '' : 'container'?>">
     <h1><?=$page->title?></h1>
